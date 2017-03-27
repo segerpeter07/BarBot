f634199e61769f7e50774941c7e52cee2bbbbe1c
segerpeter07 <segerpeter07@gmail.com>
2017-03-26 20:09:12 -0400

    EDIT: fixed flaskapp

--- flaskapp.py
+++ flaskapp.py
@@ -2,30 +2,25 @@
 Simple "Hello, World" application using Flask
 """
 
+import os
 from flask import Flask
 from flask import render_template
 from flask import request
-from tester1 import*
-flask_app = Flask('flaskapp')
+app = Flask('flaskapp')
 
 
-@flask_app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
     return render_template('index.html')
 
 
-@flask_app.route('/admin', methods=['GET', 'POST'])
-def do_me():
-    run_admin()
-
-
-@flask_app.route('/hello/', methods=['GET', 'POST'])
-@flask_app.route('/hello/<name>')
+@app.route('/hello/', methods=['GET', 'POST'])
+@app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
 
 
-@flask_app.route('/login', methods=['GET'])
+@app.route('/login', methods=['GET'])
 def login():
     if 'GET':
         return render_template('login.html')
@@ -33,12 +28,12 @@ def login():
         return render_template('hello.html')
 
 
-@flask_app.route('/profile?firstname=Peter&lastname=Seger', methods=['GET', 'POST'])
+@app.route('/profile?firstname=Peter&lastname=Seger', methods=['GET', 'POST'])
 def dashboard():
     return request.method
 
 
-@flask_app.route('/profile', methods=['POST', 'GET'])
+@app.route('/profile', methods=['POST', 'GET'])
 def profile():
     # return request.method
     firstname = request.form.get('firstname')
@@ -55,4 +50,6 @@ def profile():
 # app = flask_app.wsgi_app
 
 if __name__ == '__main__':
-    flask_app.run()
+    HOST = '0.0.0.0' if 'PORT' in os.environ else '127.0.0.1'
+    PORT = int(os.environ.get('PORT', 5000))
+    app.run(host=HOST, port=PORT)
