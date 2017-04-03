92b31b8e8ca130c23b66ed19633724e6aa8ecc2b
segerpeter07 <segerpeter07@gmail.com>
2017-04-03 15:22:18 -0400

    Added functionality to database

--- database_test.py
+++ database_test.py
@@ -10,14 +10,36 @@ to see what has been inserted
 """
 
 import sqlite3 as sql
+import sys
 
 
-def insert_account_holder(email, username, phone, password):
-        con = sql.connect("database.db")
-        cur = con.cursor()
-        cur.execute("INSERT INTO account_holder (email,username,phone,password) VALUES (?,?,?,?)", (email, username, phone, password))
-        con.commit()
-        con.close()
+def insert_user(email, username, phone, password):
+    con = sql.connect("database.db")
+    cur = con.cursor()
+    cur.execute("INSERT INTO account_holder (email,username,phone,password) VALUES (?,?,?,?)", (email, username, phone, password))
+    con.commit()
+    con.close()
 
 
-insert_account_holder('ljordan51@gmail.com', 'ljordan51', '7145107173', 'gofuckyourself')
+def return_data():
+    con = sql.connect("database.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM account_holder")
+    row = cur.fetchall()
+    print(row)
+    con.close()
+
+
+def update_info(username, password):
+    con = sql.connect('database.db')
+    cur = con.cursor()
+    cur.execute('UPDATE account_holder SET password=? WHERE username=?', (password, username))
+    con.commit()
+    con.close()
+
+
+if __name__ == '__main__':
+    # insert_account_holder('ljordan51@gmail.com', 'ljordan51', '7145107173', 'gofuckyourself')
+    # insert_user('segerpeter07@gmail.com', 'pseger', '5035446599', 'suckme')
+    update_info(input('username: '), input('password: '))
+    return_data()
