8f3c8bb124e24075a0e985fd6c44aa61b0b74164
segerpeter07 <segerpeter07@gmail.com>
2017-04-06 14:51:50 -0400

    Merge conflicts

--- flaskapp.py
+++ flaskapp.py
@@ -87,4 +87,4 @@ if __name__ == '__main__':
     HOST = '0.0.0.0' if 'PORT' in os.environ else '127.0.0.1'
     PORT = int(os.environ.get('PORT', 5000))
     # app.run(host=HOST, port=PORT)
-    app.run('10.7.10.51', port=PORT)
+    app.run('LOCALHOST', port=PORT)
